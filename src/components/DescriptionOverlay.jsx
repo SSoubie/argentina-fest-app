c1edd5816ead40c515758650ce5d49e7fdc656f4
SSoubie <ssoubie@mail.utdt.edu>
2026-02-22 21:35:25 -0300

    New description

--- src/components/DescriptionOverlay.jsx
+++ src/components/DescriptionOverlay.jsx
@@ -2,7 +2,7 @@ function DescriptionOverlay() {
   return (
     <div className="fixed top-35 left-4 bg-white bg-opacity-5 backdrop-blur-sm p-4 rounded-lg shadow-lg z-50 max-w-sm">
       <p className="text-sm leading-relaxed">
-        Argentina's carnivals are vibrant celebrations of culture, music, and tradition. From the famous parades of Corrientes to the Andean festivities of the northwest, these events showcase the country's rich diversity and artistic expression. Whant to know more? Check out <a href="https://www.argentina.travel/en/news/when-is-carnival-in-argentina-2025-and-where-is-it-celebrated" className="text-blue-500 underline">Visit Argentina</a>
+        Carnivals are a distinguished and vital expression of Argentina’s intangible cultural heritage. They blend Catholic traditions with pre-Hispanic Andean and Afro-American elements in a unique expression of music, dance, and costumes. Whant to know more? Check out <a href="https://www.argentina.travel/en/news/when-is-carnival-in-argentina-2025-and-where-is-it-celebrated" className="text-blue-500 underline">Visit Argentina</a>
       </p>
     </div>
   )
